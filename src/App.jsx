a24be7deee9c5f4a440de3c97e4e7fb3d3023b1d
ANDREHORMAN1994 <andre.horman1994@hotmail.com>
2022-11-24 21:15:37 -0300

    :heavy_check_mark: atualizando tasks

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+// 1. Listar as cores
+// 2. Renderizar o copo do suco com a cor
+// 3. Mudar a cor do copo.
+// 4. Criar o input de texto
+// 5. Armazenar as informações do input no estado
+// 6. Atualizar essas informações
+// 7. Filtrar os sucos pelas cores
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>🍹 Que Suco Foi Esse? 🍹</h1>
+      </>
+    );
+  }
+}
+
+export default App;
